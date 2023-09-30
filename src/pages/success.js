ccafb6683ed26b5cb10dd08bf41bb33eeab0f9a7
msmhmorsi <msmhmorsi@connect.ust.hk>
2023-09-30 15:11:56 +0800

    removed apos

--- src/pages/success.js
+++ src/pages/success.js
@@ -15,7 +15,7 @@ export default function SuccessPage() {
                         alt="confetti"
                         className="confetti"
                     />
-                    <h1>You're in!</h1>
+                    <h1>You&apos;re in!</h1>
                     <p>
                         You can access everything on this site. <br /> Ready to
                         get started?
